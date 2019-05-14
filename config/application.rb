98ef922fa3d9720711833939af0cc96b683ba1a9
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2019-05-14 19:47:51 +0200

    [Application] Fix available locales

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module VivatechMatchbot
     # Don't generate system test files.
     config.generators.system_tests = nil
     
+    config.i18n.available_locales = :fr
     config.i18n.default_locale = :fr
   end
 end
